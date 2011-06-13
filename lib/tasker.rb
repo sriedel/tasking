c29b3793da1ca3435c2548db275331bcab2ea6aa
Sven Riedel <sr@gimp.org>
2011-06-13 08:38:00 +0200

    Hide parent namespace variable a bit better

--- lib/tasker.rb
+++ lib/tasker.rb
@@ -5,7 +5,7 @@ module Tasker
   end
 
   def namespace( name, options = {}, &block ) 
-    parent_namespace = instance_variable_get( :@parent_namespace )
+    parent_namespace = instance_variable_get( :@__parent_namespace )
     namespace_segments = name.split( '::' )
     ns_name = parent_namespace == nil ? nil : parent_namespace.name
 
@@ -16,10 +16,10 @@ module Tasker
         ns_name = ns_segment
       end
       
-      @parent_namespace = Tasker::Namespace.new( ns_name, options, &block )
-      @parent_namespace.execute
+      @__parent_namespace = Tasker::Namespace.new( ns_name, options, &block )
+      @__parent_namespace.execute
     end
-    @parent_namespace = nil
+    @__parent_namespace = nil
   end
 end
 
