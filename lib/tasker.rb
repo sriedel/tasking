4f25cf02bbc559ba5892171a48611c3e4fd568a6
Sven Riedel <sr@gimp.org>
2011-06-13 14:45:59 +0200

    Add execute and binary

--- lib/tasker.rb
+++ lib/tasker.rb
@@ -34,7 +34,37 @@ module Tasker
     @__parent_namespace.execute
     @__parent_namespace = nil
   end
+
+  def execute( task_name )
+    namespace_segments = task_name.split( '::' )
+    task_name = namespace_segments.pop
+    namespace_name = namespace_segments.join( '::' )
+
+    task = nil
+    
+    parent_namespace = Tasker::Namespace.all.detect { |ns| ns.name == namespace_name } 
+    if parent_namespace
+      task = parent_namespace.tasks.detect { |t| t.name == task_name }
+    end
+
+    if !task && @__parent_namespace
+      namespace_name = @__parent_namespace.name + "::" + namespace_name
+      parent_namespace = Tasker::Namespace.all.detect { |ns| ns.name == namespace_name } 
+      if parent_namespace
+        task = parent_namespace.tasks.detect { |t| t.name == task_name }
+      end
+    end
+    
+    if !task
+      msg = "Unknown task '#{task_name}'"
+      msg << "or #{@__parent_namespace.name + "::" + task_name}" if @__parent_namespace
+      abort( msg )
+    end
+
+    task.execute
+  end
+  alias_method :invoke, :execute
 end
 
-require 'tasker/namespace'
-require 'tasker/task'
+require 'lib/tasker/namespace'
+require 'lib/tasker/task'
